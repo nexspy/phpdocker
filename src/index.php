ccaca4ff6c2a453bbbac9dcb53c5cca31f8851a1
Number 4 <nirmalmaswa@live.com>
2022-02-03 19:05:39 +0000

    first commit

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+echo "hello docker php projects";
+
+$mysqli = new mysqli("db", "root", "example", "company1");
+
+$sql = 'SELECT * FROM users';
+
+if ($result = $mysqli->query($sql)) {
+    while ($data = $result->fetch_object()) {
+        $users[] = $data;
+    }
+}
+
+foreach ($users as $user) {
+    echo "<br>";
+    echo $user->name . " " . $user->fav_color;
+    echo "<br>";
+}
